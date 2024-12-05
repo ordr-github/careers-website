10786824f25abc268265fe3a03c319792d720c07
ordr-github <71603350+ordr-github@users.noreply.github.com>
2024-12-05 07:09:12 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask #importing Flask Class from flask module
+from flask import Flask, render_template  #importing Flask Class from flask module
 
-app = Flask(__name__) #__name__ is a special variable in python that is automatically set to the name of the current module
+app = Flask(
+    __name__
+)  #__name__ is a special variable in python that is automatically set to the name of the current module
 
-@app.route("/") #decorator
+
+@app.route("/")  #decorator
 def hello_world():
-  return "Hello, Revl"
+  return render_template('home.html')
+
+
 if __name__ == '__main__':
-  app.run(host = "0.0.0.0", debug = True)
+  app.run(host="0.0.0.0", debug=True)
